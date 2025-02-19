92edf5da6d46d1dc9f83c83ca44b10c39c68de92
Ivan <kii1234@yandex.ru>
2025-02-19 18:36:09 +0300

    Initial commit

---
+++ src/main/java/com/kzkv/tbolimpiada/TbolimpiadaApplication.java
@@ -0,0 +1,13 @@
+package com.kzkv.tbolimpiada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TbolimpiadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TbolimpiadaApplication.class, args);
+	}
+
+}
