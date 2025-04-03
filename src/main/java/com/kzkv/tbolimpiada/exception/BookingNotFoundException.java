b063c5062b8ca351b0a3089696fb87a382a189d4
Ivan <kii1234@yandex.ru>
2025-04-03 21:52:42 +0300

    minor changes

--- src/main/java/com/kzkv/tbolimpiada/exception/BookingNotFoundException.java
+++ src/main/java/com/kzkv/tbolimpiada/exception/BookingNotFoundException.java
@@ -1,10 +1,10 @@
 package com.kzkv.tbolimpiada.exception;
 
-import java.util.UUID;
-
 public class BookingNotFoundException extends IllegalArgumentException {
 	public BookingNotFoundException(String message, Throwable cause) {
 		super(message, cause);
 	}
-	public BookingNotFoundException(String message) {}
+	public BookingNotFoundException(String message) {
+		super(message);
+	}
 }
